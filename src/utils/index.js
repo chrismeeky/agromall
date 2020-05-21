7c11638985e66184b705a6f6b0cdcd3e9277fcb9
Christian Nwodo <nwodochristian@gmail.com>
2020-05-21 14:07:57 -0400

    creates util functions for authorization and helper methods

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+import Authentication from "./authentication";
+import HelperMethods from "./helperMethod";
+import CryptData from "./cryptData";
+
+export { Authentication, HelperMethods, CryptData };
